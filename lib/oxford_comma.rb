e57c619d34ba68a9823fed1fc398202c8db5d7e9
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 16:40:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,8 @@ def oxford_comma(array)
     return array.join
     elsif array.size == 2 
       return array.join(" and ")
+      elsif array.size == 3
+      last_element = array.pop
+      array.join(", ") + last_element
   end 
 end
