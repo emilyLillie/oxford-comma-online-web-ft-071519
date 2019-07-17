b417fdf1ac9091aae92038ee9d415556581245f3
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 16:42:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    return array.join
+    elsif array.size == 2 
+      return array.join(" and ")
+    else
+      last_elem = array.pop
+	  	array.join(", ") + ", and " + last_elem
+  end 
 end
